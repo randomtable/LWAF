4be8bd8429269396da76d9caea73f31fa1e7b58c
randomtable <alefiori3@gmail.com>
2019-05-28 09:25:36 +0200

    Remove ASP.NET temporarly
    It needs more tests

--- ASP.NET/WebSite/ResultError.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class ResultError
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
-        Label1.Text = Session.Item("Result")
-    End Sub
-End Class
